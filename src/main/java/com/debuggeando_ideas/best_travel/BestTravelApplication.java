c2aa969297847fb9ec9ec193ba1a362463c803ef
IvanG <ivn.gtzs@gmail.com>
2024-10-26 14:36:13 -0600

    V35 Join con JPQL

--- src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
+++ src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
@@ -55,6 +55,12 @@ public class BestTravelApplication implements CommandLineRunner {
 		//V34 JPQL
 //		this.flyRepository.selectLessPrice(BigDecimal.valueOf(20)).forEach(f -> System.out.println(f));
 //		this.flyRepository.selectBetweenPrice(BigDecimal.valueOf(10), BigDecimal.valueOf(15)).forEach(System.out::println);
-		this.flyRepository.selectOriginDestiny("Grecia", "Mexico").forEach(System.out::println);
+//		this.flyRepository.selectOriginDestiny("Grecia", "Mexico").forEach(System.out::println);
+
+//      Primero me trae el vuelo con el Id 1, posteriormente me trae los tickets de ese vuelo con id 1
+//		var fly = flyRepository.findById(1L).get();
+//		fly.getTickets().forEach(ticket -> System.out.println(ticket));
+		var fly = flyRepository.findByTicketId(UUID.fromString("12345678-1234-5678-2236-567812345678"));
+		System.out.println(fly);
 	}
 }
