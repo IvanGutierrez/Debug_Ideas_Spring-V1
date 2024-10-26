3e2a28601e177004d460337eb8945606de44ec47
IvanG <ivn.gtzs@gmail.com>
2024-10-26 15:54:55 -0600

    V37 Join con lengueja inclusivo de Spring data

--- src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
+++ src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
@@ -67,6 +67,8 @@ public class BestTravelApplication implements CommandLineRunner {
 //		hotelRepository.findByPriceLessThan(BigDecimal.valueOf(100)).forEach(System.out::println);
 //		hotelRepository.findByPriceIsBetween(BigDecimal.valueOf(100),
 //				BigDecimal.valueOf(150)).forEach(System.out::println);
-		hotelRepository.findByRatingGreaterThan(3).forEach(System.out::println);
+//		hotelRepository.findByRatingGreaterThan(3).forEach(System.out::println);
+		var hotel = hotelRepository.findByReservationId(UUID.fromString("12345678-1234-5678-1234-567812345678"));
+		System.out.println(hotel);
 	}
 }
--- src/main/java/com/debuggeando_ideas/best_travel/domain/repositories/HotelRepository.java
+++ src/main/java/com/debuggeando_ideas/best_travel/domain/repositories/HotelRepository.java
@@ -4,11 +4,14 @@ import com.debuggeando_ideas.best_travel.domain.entities.HotelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 
 public interface HotelRepository extends JpaRepository<HotelEntity, Long> {
     //V36 Queries personalizados con lenguaje inclusivo de spring data
     Set<HotelEntity> findByPriceLessThan(BigDecimal price);
     Set<HotelEntity> findByPriceIsBetween(BigDecimal min, BigDecimal max);
     Set<HotelEntity> findByRatingGreaterThan(Integer rating);
+    Optional<HotelEntity> findByReservationId(UUID id);
 }
