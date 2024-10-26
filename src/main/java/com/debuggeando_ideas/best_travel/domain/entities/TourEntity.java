f022ebcaa81d18043f5b0d365b7c2df11ead817e
IvanG <ivn.gtzs@gmail.com>
2024-10-26 16:34:03 -0600

    V40 Mapeando relaciones inversas para tickets

--- src/main/java/com/debuggeando_ideas/best_travel/domain/entities/TourEntity.java
+++ src/main/java/com/debuggeando_ideas/best_travel/domain/entities/TourEntity.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
@@ -42,14 +44,30 @@ public class TourEntity implements Serializable {
     private CustomerEntity customer;
 
     public void addTicket(TicketEntity ticket){
+        if (Objects.isNull(this.tickets)) this.tickets = new HashSet<>();
         this.tickets.add(ticket);
     }
 
     public void removeTicket(UUID id){
+        if (Objects.isNull(this.tickets)) this.tickets = new HashSet<>();
         this.tickets.removeIf(ticket -> ticket.getId().equals(id));
     }
 
     public void updateTickets(){
         this.tickets.forEach(ticket -> ticket.setTour(this));
+    }
+
+    public void addReservation(ReservationEntity reservation){
+        if (Objects.isNull(this.reservations)) this.reservations = new HashSet<>();
+        this.reservations.add(reservation);
+    }
+
+    public void removeReservation(UUID idReservation){
+        if (Objects.isNull(this.reservations)) this.reservations = new HashSet<>();
+        this.reservations.removeIf(reservation -> reservation.getId().equals(idReservation));
+    }
+
+    public void updateReservations(){
+        this.reservations.forEach(reservation -> reservation.setTour(this));
     }
 }
